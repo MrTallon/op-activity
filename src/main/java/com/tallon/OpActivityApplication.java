9e07488edc9bdd55097591bdda3fe0f07c9dac11
tallon <352420160@qq.com>
2022-05-07 19:50:17 +0800

    feat: init commit

---
+++ src/main/java/com/tallon/OpActivityApplication.java
@@ -0,0 +1,13 @@
+package com.tallon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpActivityApplication.class, args);
+    }
+
+}
